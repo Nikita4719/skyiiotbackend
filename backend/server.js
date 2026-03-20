b6768d372fe68ba1b05ba261d7212e1ee9d5d893
Nikita4719 <nikita@skylabstech.com>
2026-03-20 17:35:34 +0530

    updateni

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
+// app.use(
+//   "/uploads",
+//   express.static(path.join(__dirname, "uploads"))
+// );
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use(
+//   "/uploads",
+//   express.static(path.join(__dirname, "../uploads"))
+// );
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+// app.use("/api/uploads", express.static(path.join(__dirname, "uploads")));
+// console.log("Serving uploads from:", path.join(__dirname, "uploads"));
 app.use("/api/header-top", headerTopRoutes);
 app.use("/api/navbar-menu", navbarMenuRoutes);
 app.use("/api/navbar-logo", navbarLogoRoutes);
@@ -74,3 +83,10 @@ app.use("/api/contact-messages", contactMessagesRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+// app.get("/check", (req, res) => {
+//   res.send("Backend working");
+// });
+// app.get("/uploads/:img", (req, res) => {
+//   const filePath = path.join(__dirname, "uploads", req.params.img);
+//   res.sendFile(filePath);
+// });
